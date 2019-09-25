0d10a6e64b013750f6ad9312e6db0d829210fa87
Richard Littauer <richard+github@burntfen.com>
2019-09-25 15:48:45 -0400

    feat: remove versions and dists, change docs

--- README.md
+++ README.md
@@ -59,21 +59,46 @@ Usage
   $ validate-maintainers <input>
 
 Options
-  --local, -l Compare a local package.json to the one in the registry
-  --dist      Print distribution tags from npm (eg: npm info <pkg> dist-tags)
-  --versions  Print versions from npm (eg: npm info <pkg> versions)
+  --local, -l   Compare a local package.json to the one in the registry
+  --commit, -c  Compare against a package.json from a particular (local)
+              commit
+  --github    Compare against a file on GitHub. Format: user/repo
+              Can be used with --commit to point to a specific commit.
+  --match     Match whatever version you are getting against the published
+              npm version
 
 Examples
-  $ validate-maintainers orbit-db
-  There are no manually-specified npm maintainers for 0.19.9.
+  $ validate-maintainers validate-maintainers
+  ✅ The \`package.json\` on npm has a valid localMaintainers field, with these maintainers:
+  - richardlitt <richard.littauer@gmail.com>
 
-  # To set a version, use <pkg@version>
+  $ validate-maintainers --commit cf5e43407cb0c682e99b01edeaaf6c43cbd27239
+  ❌ There are no manually-specified npm maintainers for validate-maintainers@1.0.0.
+
+  # To check an npm version, use <pkg@version>
   $ validate-maintainers validate-maintainers@latest
+  ✅ The \`package.json\` on npm has a valid localMaintainers field, with these maintainers:
+      - richardlitt <richard.littauer@gmail.com>
   Version: latest
-  Everybody wins!
-  The npm-set maintainers match the manually-set maintainers exactly.
-  The current maintainers for validate-maintainers@latest:
-    - richardlitt
+
+  $ validate-maintainers --commit=HEAD~5
+  ✅ The \`package.json\` at HEAD~5 has a valid localMaintainers field, with these maintainers:
+  - richardlitt <richard.littauer@gmail.com>
+
+  $ validate-maintainers --github RichardLitt/validate-maintainers
+  ✅ The \`package.json\` at RichardLitt/validate-maintainers on GitHub has a valid localMaintainers field, with these maintainers:
+  - richardlitt <richard.littauer@gmail.com>
+
+  $ validate-maintainers --github orbitdb/benchmark-runner --match
+  ✅ The \`package.json\` at orbitdb/benchmark-runner on GitHub has a valid localMaintainers field, with these maintainers:
+      - hajamark <mark@haja.io>
+  ❌ There is no \`localMaintainers\` field on npm for orbit-db-benchmark-runner@1.0.0.
+
+  Did someone publish it?
+
+  ✅ \`maintainers\` on npm matches \`localMaintainers\` in the \`package.json\` at orbitdb/benchmark-runner on GitHub exactly.
+  The current maintainer for orbit-db-benchmark-runner@1.0.0 is:
+      - hajamark
 ```
 
 ### How to set new maintainers
--- src/index.js
+++ src/index.js
@@ -9,96 +9,155 @@ const Octokit = require('@octokit/rest')
 const octokit = new Octokit()
 const base64 = require('js-base64').Base64
 
-// The meat of this program
-async function validateMaintainers (npm, flags) {
-  if (!npm) {
-    let localPackageJson = await fs.readFile('package.json', 'utf8')
-      .catch(e => {
-        if (e.code === 'ENOENT') {
-          console.log(chalk.red('You need to specify an npm package.'))
-          process.exit(1)
-        }
-      })
-    localPackageJson = JSON.parse(localPackageJson)
-    if (localPackageJson.name) {
-      npm = localPackageJson.name
-      console.log(`Checking maintainers for`, chalk.blue(`${npm}.`))
-    }
+async function getLocalPackageFromCommit (flags) {
+  const packageJson = await simpleGit.show([`${flags.commit}:package.json`])
+    .catch(() => process.exit(1)) // Error logged to console by git
+    .then(res => JSON.parse(res))
+  const pkgString = `\`package.json\` at ${flags.commit.slice(0, 7)}`
+  return [packageJson, pkgString]
+}
+
+async function getPackageFromGitHub (flags) {
+  let ghOpts = {
+    'owner': flags.github.split('/')[0], // Format: octokit/rest.js
+    'repo': flags.github.split('/')[1], // TODO Check to validate format
+    'ref': flags.commit || 'HEAD', // Should refer to main branch on GitHub. TODO test
+    'path': 'package.json' // Assume base. Add path switch if someone needs this.
   }
-  let version = npm.indexOf('@') ? npm.split('@')[1] : false
-  npm = 'https://registry.npmjs.org/' + npm.replace('@', '/')
+  const packageJson = await octokit.repos.getContents(ghOpts)
+    .catch(e => {
+      if (e.status === 404) {
+        console.log(chalk.red('That is not a GitHub repository, or we couldn\'t find that commit!'))
+        process.exit(1)
+      }
+    })
+    .then(res => JSON.parse(base64.decode(res.data.content)))
+
+  const pkgString = `\`package.json\` at ${flags.github} on GitHub${(flags.commit) ? ' at ' + flags.commit.slice(0, 7) : ''}`
+
+  return [packageJson, pkgString]
+}
+
+// Get a package.json from npm
+async function getNpmPackage (name, flags) {
+  name = 'https://registry.npmjs.org/' + name.replace('@', '/')
+
   // The package.json we fetch from NPM, which should have a maintainers field
-  const npmPackageJson = await got(npm)
-    .then(npm => JSON.parse(npm.body))
+  const packageJson = await got(name)
+    .then(name => JSON.parse(name.body))
     .catch((e) => {
+      if (e.statusCode === 405) {
+        console.log(chalk.red('405: Are you sure this is the name of the npm package?'))
+        process.exit(1)
+      }
       if (e.statusCode === 404) {
         console.log(chalk.red('404: That doesn\'t appear to be an npm package.'))
         process.exit(1)
       }
     })
 
-  // The package.json we've manually edited, either locally or fetched from npm
-  // which we compare against
-  let latest, manualPackageJson
-  if (!version) {
-    // We have to manually specify latest because npm strips out unnecessary fields
-    latest = npmPackageJson['dist-tags'].latest
-    manualPackageJson = npmPackageJson.versions[latest]
+  const pkgString = `\`package.json\` on npm`
+
+  // Return the specific version we got
+  if (flags.version) {
+    return [packageJson, pkgString]
+  }
+
+  // Or just return the latest one
+  // We have to manually specify latest because npm strips out unnecessary fields
+  return [packageJson.versions[packageJson['dist-tags'].latest], pkgString]
+}
+
+// Get the local file
+async function getLocalPackage () {
+  let packageJson = await fs.readFile('package.json', 'utf8')
+    .catch(e => {
+      if (e.code === 'ENOENT') {
+        console.log(chalk.red('There is no package.json in this directory.'))
+        process.exit(1)
+      }
+    })
+  packageJson = JSON.parse(packageJson)
+  return [packageJson, `local \`package.json\``]
+}
+
+async function matchMaintainers (npmPackageJson, sourcePackageJson, pkgString) {
+  const npmField = JSON.stringify(_.map(npmPackageJson.maintainers, (obj) => helpers.sortKeys(obj)).sort(helpers.sortAlphabetic))
+  const sourceField = JSON.stringify(_.map(helpers.convertStringToArr(sourcePackageJson.localMaintainers), (user) => helpers.sortKeys(parse(user))).sort(helpers.sortAlphabetic))
+
+  if (!npmPackageJson.localMaintainers) {
+    console.log(`❌ There is no \`localMaintainers\` field on npm for ${npmPackageJson.name}@${npmPackageJson.version || npmPackageJson['dist-tags'].latest}.
+
+Did someone publish it?
+`)
+  }
+
+  if (npmField === sourceField) {
+    console.log(`✅ \`maintainers\` on npm ${chalk.green('matches')} \`localMaintainers\` in the ${pkgString} exactly.
+The current maintainer${(npmPackageJson.maintainers.length === 1) ? '' : 's'} for ${npmPackageJson.name}@${npmPackageJson.version || npmPackageJson['dist-tags'].latest} ${(npmPackageJson.maintainers.length === 1) ? 'is' : 'are'}:
+    - ${_.map(npmPackageJson.maintainers, (user) => user.name).join('\n    - ')}`)
   } else {
-    manualPackageJson = npmPackageJson
+    console.log(`❌ There are \`localMaintainers\` in the ${pkgString}, but they don't match the ones on npm.`)
+    console.log('npm field: ', chalk.red(`${helpers.stringifyUsers(npmPackageJson.maintainers)}`))
+    console.log('local field:', chalk.red(`${helpers.stringifyUsers(sourcePackageJson.localMaintainers)}`))
   }
+}
 
-  // If a local packageJson is specified, read it locally, and reset the manual var
-  if (flags.local) {
-    let localPackageJson = await fs.readFile('package.json', 'utf8')
-    manualPackageJson = JSON.parse(localPackageJson)
-  }
+// The meat of this program
+async function validateMaintainers (name, flags) {
+  let packageJson, npmPackageJson, pkgString, rest
+  flags.version = flags.version || (name && name.indexOf('@')) ? name.split('@')[1] : false
 
-  // If we are getting the manual package.json from a local git commit
+  // Get the Package.Json to check
+  // If there is a commit flag, get that in the current git directory
   if (flags.commit && !flags.github) {
-    let localPackageJson = await simpleGit.show([`${flags.commit}:package.json`])
-    manualPackageJson = JSON.parse(localPackageJson)
+    [packageJson, pkgString] = await getLocalPackageFromCommit(flags)
+
+  // If a package is not specified as an argument, check for a local file
+  } else if (flags.local || !(name || flags.github)) {
+    [packageJson, pkgString] = await getLocalPackage()
+
+  // Error case
+  } else if (flags.github && flags.local && !flags.match) {
+    console.log(chalk.red('You cannot specify both GitHub and a local file without using --match'))
+    process.exit(1)
+
+  // If there is a GitHub flag, check against that (flags.commit can be there, too)
+  } else if (flags.github) {
+    [packageJson, pkgString] = await getPackageFromGitHub(flags)
+
+  // Else, fetch from npm
+  } else {
+    [packageJson, pkgString] = await getNpmPackage(name, flags)
+    flags.match = 'identical'
+    // Set this so we can get specific information if needed
+    npmPackageJson = packageJson
   }
 
-  // Fetch a GitHub commit package if specified
-  if (flags.github) {
-    let [ owner, repo ] = flags.github.split('/') // Format: octokit/rest.js
-    let ref = flags.commit || 'HEAD' // Should refer to main branch on GitHub. TODO test
-    let path = 'package.json' // Assume base. Add path switch if someone needs this.
-    let localPackageJson = await octokit.repos.getContents({ owner, repo, path, ref })
-    manualPackageJson = JSON.parse(base64.decode(localPackageJson.data.content))
+  if (!packageJson.localMaintainers) {
+    console.log(chalk.red(`❌ There are no manually-specified npm maintainers for the ${pkgString}.`))
+    process.exit(1)
+  } else {
+    console.log(`✅ The ${chalk.blue(pkgString)} has a ${chalk.green('valid')} localMaintainers field, with these maintainers:
+    - ${_.map(_.map(helpers.convertStringToArr(packageJson.localMaintainers), (user) => helpers.sortKeys(parse(user))).sort(helpers.sortAlphabetic), (user) => `${user.name} <${user.email}>`).join('\n    - ')}`)
+  }
+
+  // Get the Npm package, if needed
+  if (!npmPackageJson && (flags.version)) {
+    [npmPackageJson, ...rest] = await getNpmPackage(packageJson.name, flags)
   }
 
   // Show me current releases, and whatever version I've specified
-  if (version) {
-    console.log(`Version: ${version}`)
+  if (flags.version) {
+    console.log(`Version: ${flags.version}`)
   }
 
-  if (flags.versions) {
-    console.log('Versions: ' + chalk.blue(JSON.stringify(Object.keys(npmPackageJson['versions']), null, 2)))
-  }
+  if (flags.match && typeof flags.match !== 'string') {
+    if (!npmPackageJson) {
+      [npmPackageJson, ...rest] = await getNpmPackage(packageJson.name, flags)
+    }
 
-  if (flags.dist) {
-    console.log('Dist tags: ' + chalk.blue(JSON.stringify(npmPackageJson['dist-tags'], null, 2)))
-  }
-
-  if (!manualPackageJson.localMaintainers) {
-    console.log(chalk.red(`There are no manually-specified npm maintainers for local ${manualPackageJson.name}@${manualPackageJson.version || manualPackageJson['dist-tags'].latest}.`))
-    process.exit(1)
-  }
-
-  const npmField = JSON.stringify(_.map(npmPackageJson.maintainers, (obj) => helpers.sortKeys(obj)).sort(helpers.sortAlphabetic))
-  const localField = JSON.stringify(_.map(helpers.convertStringToArr(manualPackageJson.localMaintainers), (user) => helpers.sortKeys(parse(user))).sort(helpers.sortAlphabetic))
-
-  if (npmField === localField) {
-    console.log(chalk.green(`Everybody wins!
-The npm-set maintainers match the manually-set maintainers exactly.
-The current maintainers for ${npmPackageJson.name}@${version || npmPackageJson['dist-tags'].latest}:
-  - ${_.map(npmPackageJson.maintainers, (user) => user.name).join('\n   - ')}`))
-  } else {
-    console.log(`There are manually-specified maintainers, but they don't match the ones on NPM.`)
-    console.log('npm field: ', chalk.red(`${helpers.stringifyUsers(npmPackageJson.maintainers)}`))
-    console.log('local field:', chalk.red(`${helpers.stringifyUsers(manualPackageJson.localMaintainers)}`))
+    matchMaintainers(npmPackageJson, packageJson, pkgString)
   }
 }
 
